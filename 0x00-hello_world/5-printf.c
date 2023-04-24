1e1bd17157c145b412c8fe198f667305fbc3dc59
ella97 <nuwymwanri@gmail.com>
2023-04-24 11:25:49 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,3 +4,16 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+int main(void)
+{int a;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+int main(void)
+{int a;
+}
