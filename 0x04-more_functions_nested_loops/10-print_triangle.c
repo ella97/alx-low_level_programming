bd380605ed350a8ca540199f3fc5eb9562d7d126
ella97 <nuwymwanri@gmail.com>
2023-04-27 11:35:32 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+void print_triangle(int size)
+{
+int row, col;
+if (size <= 0) {
+_putchar(n);
+return;
+}
+for (row = 1; row <= size; row++) {
+for (col = 1; col <= row; col++) {
+_putchar(#);
+}
+_putchar(n);
+}
+}
