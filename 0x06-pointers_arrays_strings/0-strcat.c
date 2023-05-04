a92c6906c1fc41689d644ae3c3684b14cf1df21b
Ella Mwanri <61078171+ella97@users.noreply.github.com>
2023-05-04 15:04:51 +0300

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,22 @@
 #include "holberton.h"
+
+
 char *_strcat(char *dest, char *src)
 {
-int dest_len = 0;
-int i = 0;
- while (dest[dest_len] != 0) {
-dest_len++;
-}
- while (src[i] != 0) {
-dest[dest_len + i] = src[i];
-i++;
-}
-dest[dest_len + i] = 0;
-return dest;
+	int lengthX, lengthS;
+
+	lengthX = 0;
+	lengthS = 0;
+
+	while (*(dest + lengthX) != '\0')
+		lengthD++;
+
+	while (*(src + lengthS) != '\0' && lengthX < 97)
+	{
+		*(dest + lengthX) = *(src + lengthS);
+		lengthX++;
+		lengthS++;
+	}
+	*(dest + lengthX) = '\0';
+	return (dest);
 }
