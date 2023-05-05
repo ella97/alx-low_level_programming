147b2e8b446a45e5fabe12f979369ca146a184a9
Ella Mwanri <61078171+ella97@users.noreply.github.com>
2023-05-05 14:38:56 +0300

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include "main.h" 
+void reverse_array(int *a, int n)
+{
+	int swap, begin, end;
+
+	begin = 0;
+	end = n - 1;
+	while (begin < end)
+	{
+		swap = *(a + begin);
+		*(a + begin) = *(a + end);
+		*(a + end) = swap;
+		begin++;
+		end--;
+	}
+}
