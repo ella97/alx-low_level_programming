836eab57805663c16ceb0be57336e668702461d7
ella97 <nuwymwanri@gmail.com>
2024-06-28 16:34:13 +0300

    Changelog: New Corrections

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,41 +1,50 @@
 #ifndef _SORTING_ALGOS_H_
 #define _SORTING_ALGOS_H_
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * struct listint_s - Doubly linked list node
+ * struct listint_s - singly linked list
  *
- * @n: Integer stored in the node
- * @next: Pointer to the next element of the list
- * @index: Index of the node
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
  */
-
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 /**
- * struct listskip_s - Doubly linked list node
+ * struct skiplist_s - Singly linked list with an exp lane
  *
- * @n: Integer stored in the node
- * @index: Index of the node
- * @next: Pointer to the next element of the list
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
  * @exp: Pointer to the next node in the exp lane
+ *
+ * Description: singly linked list node structure 
  */
-
-typedef struct listskip_s
+typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct listskip_s *next;
-    struct listskip_s *exp;
-} listskip_t;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *exp;
+} skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
